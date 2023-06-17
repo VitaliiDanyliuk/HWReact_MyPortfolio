d07a5db7448cbc08e879b9e1d822877414989804
VitaliiDanyliuk <danyliuk.vitalii@gmail.com>
2023-06-17 22:03:35 +0300

    done

---
+++ src/components/helpers/projectsList.js
@@ -0,0 +1,33 @@
+import project1 from "../../img/projects/01.png";
+import project1Big from "../../img/projects/01-big.png";
+import project2 from "../../img/projects/02.png";
+import project2Big from "../../img/projects/02-big.png";
+import project3 from "../../img/projects/03-big.png";
+import project3Big from "../../img/projects/03-big.png";
+
+
+const projects = [
+  {
+    title: "Clothing store",
+    img: project1,
+    imgBig: project1Big,
+    gitHubLink:
+      "https://github.com/VitaliiDanyliuk/7eHomework/tree/master/Homework%20JS%2011.12.22%20Figma",
+  },
+  {
+    title: "MissCupcape",
+    img: project2,
+    imgBig: project2Big,
+    gitHubLink:
+      "https://github.com/VitaliiDanyliuk/7eHomework/tree/master/HW%20JS%20Miss%20cupcace",
+  },
+  {
+    title: "TodoList",
+    img: project3,
+    imgBig: project3Big,
+    gitHubLink:
+      "https://github.com/VitaliiDanyliuk/7eHomeworkReact/tree/master/hw_react_third",
+  },
+];
+
+export {projects}
---
+++ src/pages/Projects/index.jsx
@@ -0,0 +1,23 @@
+
+import Project from "../../components/Project";
+
+
+import { projects} from "../../components/helpers/projectsList"
+
+const Projects = () => {
+    return ( 
+         <main className="section">
+        <div className="container">
+            <h2 className="title-1">Projects</h2>
+                <ul className="projects">
+                    {projects.map((project, index) => { 
+                        return (<Project key={index + Math.random() * 1000} title={project.title} img={project.img} index={index} />)
+                    })}
+               
+            </ul>
+        </div>
+    </main>
+     );
+}
+ 
+export default Projects;
